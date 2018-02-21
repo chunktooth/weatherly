817b358da9be2208aea93e86170d7b2fdd0e325a
chunktooth <om.saetehn@gmail.com>
2018-02-20 18:27:04 -0700

    Skeletons

---
+++ CurrentWeather.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+export default class CurrentWeather extends Component {
+	constructor() {
+		super();
+
+	}
+
+	render() {
+		return (
+				<div className="Current">
+					<h1>WHAT UP</h1>
+				</div>
+			);
+	}
+}
